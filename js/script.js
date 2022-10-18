30ee16b84dc4d5ba43b6424f7dd9ee21844a92bd
gneisscode <103408439+gneisscode@users.noreply.github.com>
2022-10-18 18:10:26 +0200

    added menu files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function openNav() {
+  let hamburgerMenu = document.getElementById("nav-hamburger-menu");
+  hamburgerMenu.className = "hamburger__menu__active";
+  hamburgerMenu.style.width = "296px";
+}
+
+function closeNav() {
+  let hamburgerMenu = document.getElementById("nav-hamburger-menu");
+  hamburgerMenu.className = "nav__hamburger__menu";
+  hamburgerMenu.style.width = "0";
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
